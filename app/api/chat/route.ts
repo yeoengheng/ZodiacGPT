e0bfa8f13fcb008184a1db88930b4d80bb452599
Eng Heng <enghengyeo12@Hotmail.com>
2024-01-23 20:10:36 +0800

    Added a rephraser to improve prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -31,10 +31,26 @@ export async function POST(req: Request) {
   }
 
   const length = messages.length
+  // rephraser here
+  async function rephraseInput(inputString:string) {
+    const gptAnswer = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a rephraser and always respond with a rephrased version of the input that is given to a Chinese Fortune Teller. Ensure it's always relevant to the year 2024. Always be succint and use the same words as the input.",
+        },
+        { role: "user", content: inputString },
+      ],
+    });
+    return gptAnswer.choices[0].message.content;
+  }
+  const rephrase = await rephraseInput(messages[length-1].content) as string 
 
   const embeddingResponse = await openai.embeddings.create({
     model:'text-embedding-ada-002',
-    input:messages[length-1].content
+    input:rephrase
   })
   const [{embedding}] = embeddingResponse.data
   const {data:docs} = await supabase.rpc('match_zodiac',{
